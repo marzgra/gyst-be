e22e53fb4a06430c43e8d4759ded26ba49e06e95
marzgra <marzgra@o2.pl>
2025-05-16 22:09:00 +0200

    create user - tests

---
+++ src/test/java/com/gyst/TestConstants.java
@@ -0,0 +1,8 @@
+package com.gyst;
+
+public class TestConstants {
+    public static final Long USER_ID = 42L;
+    public static final String USER_NAME = "Alice";
+    public static final String USER_EMAIL = "alice@domain.com";
+
+}
---
+++ src/test/java/com/gyst/application/user/RegisterUserUserCaseTest.java
@@ -0,0 +1,30 @@
+package com.gyst.application.user;
+
+import com.gyst.domain.user.model.User;
+import com.gyst.domain.user.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static com.gyst.TestConstants.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+
+class RegisterUserUseCaseTest {
+
+    @Test
+    void shouldRegisterUserAndReturnId() {
+        // given
+        UserRepository mockRepo = Mockito.mock(UserRepository.class);
+        User savedUser = User.builder().id(USER_ID).name(USER_NAME).email(USER_EMAIL).build();
+        Mockito.when(mockRepo.save(any(User.class))).thenReturn(savedUser);
+
+        RegisterUserUseCase useCase = new RegisterUserUseCase(mockRepo);
+
+        // when
+        Long result = useCase.register(USER_EMAIL, USER_NAME);
+
+        // then
+        assertEquals(USER_ID, result);
+    }
+}
+
